769868708d47a246e69353892ebc74da83ad4dbf
Mikhail Mustakimov <mikhail@mustakimov.ru>
2020-01-06 21:49:59 +0800

    Add main function

--- src/main/kotlin/ru/mustakimov/pascal/Main.kt
+++ src/main/kotlin/ru/mustakimov/pascal/Main.kt
@@ -1,5 +1,28 @@
 package ru.mustakimov.pascal
 
-fun main(args: Array<String>) {
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import kotlin.system.exitProcess
 
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Simple pascal interpreter.\nUSAGE: <EXECUTABLE> <FILENAME>")
+        println("Where:\n\t<EXECUTABLE> - this file (often java -jar pascal-interpreter.jar)")
+        println("\t<FILENAME> - file to be interpreted")
+        exitProcess(1)
+    }
+
+    val filename = args.first()
+    val file = File(filename)
+    if (!file.exists() || !file.isFile) {
+        println("You should specify correct file. Cannot open file")
+    }
+    val content = BufferedReader(FileReader(file)).readText()
+    val interpreter = Interpreter.newInstance(content)
+    interpreter.interpret()
+    println("Used variables:")
+    interpreter.variables.forEach {
+        println("\t${it.key} = ${it.value}")
+    }
 }
